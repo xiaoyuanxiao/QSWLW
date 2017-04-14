55decc77e2fcb1e2210a2a03ecb8ba91c35551ba
xiaoyu <2640171401@qq.com>
2017-04-14 15:51:44 +0800

    e

--- app/src/main/java/com/qs/qswlw/activity/MainActivity.java
+++ app/src/main/java/com/qs/qswlw/activity/MainActivity.java
@@ -3,7 +3,6 @@ package com.qs.qswlw.activity;
 import android.app.AlertDialog;
 import android.content.Intent;
 import android.graphics.Color;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.webkit.WebView;
