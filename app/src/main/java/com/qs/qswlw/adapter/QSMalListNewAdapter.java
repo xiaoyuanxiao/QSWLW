de5a844d2e0921a8f9045caede5ae60f141892e3
xiaoyu <2640171401@qq.com>
2017-05-03 11:50:44 +0800

    ...

--- app/src/main/java/com/qs/qswlw/adapter/QSMalListNewAdapter.java
+++ app/src/main/java/com/qs/qswlw/adapter/QSMalListNewAdapter.java
@@ -1,18 +1,27 @@
 package com.qs.qswlw.adapter;
 
 import android.content.Context;
+import android.os.Handler;
+import android.os.Message;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.qs.qswlw.R;
+
 
 /**
  * Created by xiaoyu on 2017/4/26.
  */
 
-public class QSMalListNewAdapter extends BaseAdapter {
+public class QSMalListNewAdapter extends BaseAdapter implements View.OnClickListener {
     private Context context;
+    private int number = 0;
+    private EditText edt_num;
+    private TextView tv_minus, tv_add;
+
 
     public QSMalListNewAdapter(Context context) {
         this.context = context;
@@ -35,7 +44,43 @@ public class QSMalListNewAdapter extends BaseAdapter {
 
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
-        view =  View.inflate(context, R.layout.item_qsmalllist,null);
+
+        view = View.inflate(context, R.layout.item_qsmalllist, null);
+        edt_num = (EditText) view.findViewById(R.id.edt_num);
+        tv_minus = (TextView) view.findViewById(R.id.tv_minus);
+        tv_add = (TextView) view.findViewById(R.id.tv_add);
+        String str = edt_num.getText().toString();
+        number = Integer.valueOf(str);
+        tv_minus.setOnClickListener(this);
+        tv_add.setOnClickListener(this);
         return view;
     }
+
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            if (msg.what == 2) {//更改对话框中的数量
+                edt_num.setText(String.valueOf(number));
+            }
+        }
+    };
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.tv_minus:
+                if (number > 1) {
+                    number--;
+                    handler.sendEmptyMessage(2);
+                }
+                break;
+            case R.id.tv_add:
+                number++;
+                handler.sendEmptyMessage(2);
+                break;
+        }
+    }
+
+
 }
--- app/src/main/java/com/qs/qswlw/fragment/CommodityDetailsWareFragment.java
+++ app/src/main/java/com/qs/qswlw/fragment/CommodityDetailsWareFragment.java
@@ -1,6 +1,9 @@
 package com.qs.qswlw.fragment;
 
+import android.os.Handler;
+import android.os.Message;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.qs.qswlw.R;
@@ -9,10 +12,12 @@ import com.qs.qswlw.R;
  * Created by xiaoyu on 2017/4/28.
  */
 
-public class CommodityDetailsWareFragment extends BaseFragment {
+public class CommodityDetailsWareFragment extends BaseFragment implements View.OnClickListener {
 
     private TextView tv_fg_ware;
-
+    private TextView tv_minus,tv_add;
+    private EditText edt_num;
+    private int number = 0;
     public static CommodityDetailsWareFragment newInstance() {
         CommodityDetailsWareFragment fragment = new CommodityDetailsWareFragment();
         return fragment;
@@ -21,7 +26,15 @@ public class CommodityDetailsWareFragment extends BaseFragment {
     @Override
     View initView() {
         View inflate = View.inflate(activity, R.layout.fg_commoditydetailsware, null);
+        tv_minus = (TextView) inflate.findViewById(R.id.tv_minus);
+        tv_add = (TextView) inflate.findViewById(R.id.tv_add);
+        edt_num = (EditText) inflate.findViewById(R.id.edt_num);
         tv_fg_ware = (TextView) inflate.findViewById(R.id.tv_fg_ware);
+        String str = edt_num.getText().toString();
+        number = Integer.valueOf(str);
+        tv_minus.setOnClickListener(this);
+        tv_add.setOnClickListener(this);
+
         return inflate;
     }
 
@@ -29,5 +42,30 @@ public class CommodityDetailsWareFragment extends BaseFragment {
     protected void initData() {
         super.initData();
 
+    }
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            if (msg.what == 2) {//更改对话框中的数量
+                edt_num.setText(String.valueOf(number));
+            }
+        }
+    };
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.tv_minus:
+                if (number > 1) {
+                    number--;
+                    handler.sendEmptyMessage(2);
+                }
+                break;
+            case R.id.tv_add:
+                number++;
+                handler.sendEmptyMessage(2);
+                break;
+        }
     }
 }
