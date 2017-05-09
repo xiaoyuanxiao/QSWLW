077663d356344a6e473f0e55a0904fb34e863622
xiaoyu <2640171401@qq.com>
2017-05-09 17:18:07 +0800

    促销抽奖

--- app/src/main/java/com/qs/qswlw/activity/LuckGameActivity.java
+++ app/src/main/java/com/qs/qswlw/activity/LuckGameActivity.java
@@ -4,15 +4,20 @@ import android.view.View;
 
 import com.qs.qswlw.R;
 import com.qs.qswlw.activity.PersonalCenter.BaseInfoActivity;
+import com.qs.qswlw.view.MyGifView;
 
 /**
  * Created by xiaoyu on 2017/5/9.
  */
 
 public class LuckGameActivity extends BaseInfoActivity {
+
+    private MyGifView gif_luckgame;
+
     @Override
     public View setConetnView() {
         View inflate = View.inflate(this, R.layout.activity_luckgame, null);
+        gif_luckgame = (MyGifView) inflate.findViewById(R.id.gif_luckgame);
         return inflate;
     }
 
@@ -21,5 +26,11 @@ public class LuckGameActivity extends BaseInfoActivity {
         super.initfindviewByid();
         tv_titlebar_center.setText("促销抽奖");
         tv_titlebar_right.setText("抽奖记录");
+    }
+
+    @Override
+    public void initData() {
+        super.initData();
+        gif_luckgame.setMovieResource(R.raw.chouqian);
     }
 }
