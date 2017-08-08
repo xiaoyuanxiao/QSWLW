71870c967b03b1eae6687bace4bf079ca84b871e
xiaoyu <2640171401@qq.com>
2017-08-08 14:32:12 +0800

    dialog

--- app/src/main/java/com/qs/qswlw/activity/PersonalCenter/EntrepreneurialDonationActivity.java
+++ app/src/main/java/com/qs/qswlw/activity/PersonalCenter/EntrepreneurialDonationActivity.java
@@ -2,15 +2,13 @@ package com.qs.qswlw.activity.PersonalCenter;
 
 import android.app.AlertDialog;
 import android.content.Intent;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import com.qs.qswlw.R;
-import com.qs.qswlw.view.WheelView;
-
-import java.util.Arrays;
 
 /**
  * Created by xiaoyu on 2017/4/6.
@@ -19,7 +17,10 @@ import java.util.Arrays;
 public class EntrepreneurialDonationActivity extends BaseInfoActivity {
 
     private TextView tv_entrepreneurialdonation;
-    private static final String[] PLANETS = new String[]{"创业基金", "创新基金"};
+    private RadioGroup rg_entrepreneurialdialog;
+    private RadioButton radio_top,radio_bellow;
+    private AlertDialog dialog;
+    // private static final String[] PLANETS = new String[]{"创业基金", "创新基金"};
 
     @Override
     public View setConetnView() {
@@ -41,6 +42,7 @@ public class EntrepreneurialDonationActivity extends BaseInfoActivity {
     public void setOnclick() {
         super.setOnclick();
         tv_entrepreneurialdonation.setOnClickListener(this);
+
     }
 
     @Override
@@ -48,25 +50,35 @@ public class EntrepreneurialDonationActivity extends BaseInfoActivity {
         super.onClick(v);
         switch (v.getId()) {
             case R.id.tv_entrepreneurialdonation:
-                View outerView = LayoutInflater.from(this).inflate(R.layout.wheel_view, null);
-                WheelView wv = (WheelView) outerView.findViewById(R.id.wheel_view_wv);
-                //  wv.setOffset(1);
-                wv.setItems(Arrays.asList(PLANETS));
-                wv.setSeletion(0);
-                wv.setOnWheelViewListener(new WheelView.OnWheelViewListener() {
-                    @Override
-                    public void onSelected(int selectedIndex, String item) {
-                        Log.d("tag", "[Dialog]selectedIndex: " + selectedIndex + ", item: " + item);
-                    }
-                });
-                final AlertDialog.Builder builder = new AlertDialog.Builder(this);
-                final AlertDialog dialog = builder.create();
-                dialog.setView(outerView);
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                dialog = builder.create();
+                View dialogView = LayoutInflater.from(EntrepreneurialDonationActivity.this).inflate(R.layout.entrepreneurialdialog, null);
+                rg_entrepreneurialdialog = (RadioGroup) dialogView.findViewById(R.id.rg_entrepreneurialdialog);
+                rg_entrepreneurialdialog.setOnCheckedChangeListener(new CheckedChangeListener());
+                radio_top = (RadioButton) dialogView.findViewById(R.id.radio_top);
+                radio_bellow = (RadioButton) dialogView.findViewById(R.id.radio_bellow);
+                radio_top.setOnClickListener(this);
+                radio_bellow.setOnClickListener(this);
+                dialog.setView(dialogView);
                 dialog.show();
-              break;
-            case R.id.tv_titlebar_right:
-                startActivity(new Intent(this,EntrepreneurialDonationRecordActivity.class));
                 break;
+            case R.id.tv_titlebar_right:
+                startActivity(new Intent(this, EntrepreneurialDonationRecordActivity.class));
+                break;
+        }
+    }
+
+    class CheckedChangeListener implements RadioGroup.OnCheckedChangeListener{
+
+        @Override
+        public void onCheckedChanged(RadioGroup radioGroup, int i) {
+            if(i==radio_top.getId()){
+                tv_entrepreneurialdonation.setText(radio_top.getText());
+                dialog.dismiss();
+            }else if(i==radio_bellow.getId()){
+                tv_entrepreneurialdonation.setText(radio_bellow.getText());
+                dialog.dismiss();
+            }
         }
     }
 
