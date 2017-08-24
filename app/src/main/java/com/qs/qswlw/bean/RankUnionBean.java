4155d35b0ef0a29878cb5fa162ddcb48bb9f831f
xiaoyu <2640171401@qq.com>
2017-08-24 17:03:18 +0800

    联盟商家销售额排行榜

---
+++ app/src/main/java/com/qs/qswlw/bean/RankUnionBean.java
@@ -0,0 +1,84 @@
+package com.qs.qswlw.bean;
+
+import java.util.List;
+
+/**
+ * Created by xiaoyu on 2017/8/24.
+ */
+
+public class RankUnionBean {
+    /**
+     * 联盟商家销售额排行榜
+     */
+
+    /**
+     * msg : 成功！
+     * result : {"i":1,"single_log":[{"sum_money":"11039330.00","user_id":"8895"},{"photo":"/Uploads/proof/2017-08-16/5993f0941f66f.jpg","store_name":"常熟市古里镇恒洁卫浴经营部","sum_money":"10750544.00","user_id":"3709"},{"photo":"/Uploads/proof/2017-08-16/5993957f6fc4a.jpg","store_name":"金勺山茶油","sum_money":"9827740.00","user_id":"4926"},{"photo":"/Uploads/proof/2017-08-17/5995163f58b04.jpg","store_name":"格莱美陶瓷","sum_money":"8469120.00","user_id":"2558"},{"photo":"/Uploads/proof/2017-05-11/5914144f75a00.jpg","store_name":"方圆老酒店","sum_money":"7440352.00","user_id":"7046"},{"photo":"/Uploads/proof/2017-05-11/59141199efeeb.jpg","store_name":"养生保健咨询","sum_money":"6838000.00","user_id":"3509"},{"photo":"/Uploads/proof/2017-05-06/590dc5ae9214c.png","store_name":"茏瑾","sum_money":"6698800.00","user_id":"5243"},{"photo":"/Uploads/proof/2017-05-07/590e978227f4e.jpg","store_name":"新兴纺织品批发部","sum_money":"5286900.00","user_id":"296"},{"photo":"/Uploads/proof/2017-08-20/59995bda4b919.jpg","store_name":"上海超赞投资管理有限公司","sum_money":"4689300.00","user_id":"5060"},{"photo":"/Uploads/proof/2017-08-16/59939ed51c1f3.jpg","store_name":"善乐针织羊毛衫","sum_money":"4130354.00","user_id":"679"},{"photo":"/Uploads/proof/2017-08-18/59963496700fd.JPG","store_name":"寿昌综合商店","sum_money":"3946992.00","user_id":"1923"},{"photo":"/Uploads/proof/2017-08-16/5993a573d2bd3.jpg","store_name":"知民大酒店","sum_money":"3935140.00","user_id":"1715"},{"photo":"/Uploads/proof/2017-08-18/599636691718a.JPG","store_name":"寿昌综合商店","sum_money":"3912144.00","user_id":"5345"},{"photo":"/Uploads/proof/2017-08-15/5992c995a3af3.jpg","store_name":"江都市仙女镇培罗蒙服饰店","sum_money":"3647340.00","user_id":"2052"},{"photo":"/Uploads/proof/2017-08-16/5993d559965ff.jpg","store_name":"全民大药房","sum_money":"3575090.00","user_id":"4796"},{"photo":"/Uploads/proof/2017-05-05/590c2f2b44f0a.jpg","store_name":"高邮北海之家","sum_money":"3523420.80","user_id":"842"},{"photo":"/Uploads/proof/2017-08-19/5997b9bec59a5.jpg","store_name":"昆明中国国际旅行社春城路分社","sum_money":"3421935.00","user_id":"1807"},{"photo":"/Uploads/proof/2017-08-15/59927b816b16c.jpg","store_name":"赵怡甜服装商行","sum_money":"3287856.00","user_id":"2687"},{"photo":"/Uploads/proof/2017-08-17/599534e20e46b.jpg","store_name":"小河索美达办公用品","sum_money":"3159024.00","user_id":"4974"},{"photo":"/Uploads/proof/2017-08-16/5993b19138301.jpg","store_name":"四季飘香饭馆","sum_money":"3127874.00","user_id":"684"},{"photo":"/Uploads/proof/2017-08-15/5992e1993be92.jpg","store_name":"高邮市明宇汽车配件经营部","sum_money":"3106100.00","user_id":"1855"},{"photo":"/Uploads/proof/2017-08-15/599267dc7940a.jpg","store_name":"绿辉果品","sum_money":"3081200.00","user_id":"2577"},{"photo":"/Uploads/proof/2017-05-23/5923a39e5542a.JPG","store_name":"上海干艺文化传播工作室","sum_money":"3074816.00","user_id":"2125"},{"photo":"/Uploads/proof/2017-08-15/5992f7a1664b7.jpg","store_name":"四通粮油店","sum_money":"3054212.00","user_id":"1910"},{"photo":"/Uploads/proof/2017-04-11/58eca27825d68.png","store_name":"新晟食品","sum_money":"2993537.00","user_id":"193"},{"photo":"/Uploads/proof/2017-08-15/5992682ea6b00.jpg","store_name":"泽天秀育发馆","sum_money":"2964000.00","user_id":"2727"},{"photo":"/Uploads/proof/2017-05-12/59154d6ed3838.jpg","store_name":"闻名天虾","sum_money":"2946925.20","user_id":"571"},{"photo":"","store_name":"好屋来皮鞋店","sum_money":"2930400.00","user_id":"4418"},{"photo":"/Uploads/proof/2017-06-16/59434cf8422b5.jpg","store_name":"虞城旅游","sum_money":"2834800.00","user_id":"3009"},{"photo":"/Uploads/proof/2017-05-11/591451a1baa06.jpg","store_name":"弘阳","sum_money":"2819600.00","user_id":"8221"},{"photo":"/Uploads/proof/2017-08-17/59950de116171.jpg","store_name":"南北码头食品","sum_money":"2769154.00","user_id":"8488"},{"photo":"/Uploads/proof/2017-08-14/5991690e92063.jpg","store_name":"水清木秀美容院","sum_money":"2739950.00","user_id":"2596"},{"photo":"/Uploads/proof/2017-08-19/59983ad5a95a0.jpg","store_name":"友谊商店","sum_money":"2628912.00","user_id":"3837"},{"photo":"/Uploads/proof/2017-08-22/599be2d41c072.JPG","store_name":"武汉荣昌仁和会计咨询服务有限公司昆山分公司","sum_money":"2567190.00","user_id":"1516"},{"photo":"/Uploads/proof/2017-08-16/5993a4eea57a8.JPG","store_name":"江苏中惠医疗科技股份有限公司","sum_money":"2535754.00","user_id":"1794"},{"photo":"/Uploads/proof/2017-08-16/5993d68e3962f.jpg","store_name":"满芬百货商店","sum_money":"2510112.00","user_id":"3198"},{"photo":"/Uploads/proof/2017-05-04/590ad488c827b.jpg","store_name":"昆山市玉山镇庆弟汽配商行","sum_money":"2486756.00","user_id":"4162"},{"photo":"/Uploads/proof/2017-05-30/592cbf08770ba.jpg","store_name":"昆山宝盛汽车服务有限公司","sum_money":"2465069.00","user_id":"1800"},{"photo":"/Uploads/proof/2017-08-15/5992e6d733a36.jpg","store_name":"高邮市明宇汽车配件经营部","sum_money":"2451428.00","user_id":"1888"},{"photo":"/Uploads/proof/2017-08-16/5993bc2b10372.jpg","store_name":"扬州市邗江区东茂酒业商行","sum_money":"2446000.00","user_id":"1882"},{"photo":"/Uploads/proof/2017-08-21/599a36244de6b.jpg","store_name":"江都区仙女镇天年纺织品经营部","sum_money":"2438900.00","user_id":"3445"},{"photo":"/Uploads/proof/2017-08-17/59953fcc50170.jpg","store_name":"江都区邵伯镇育才文体用品经营部","sum_money":"2337956.00","user_id":"3118"},{"photo":"/Uploads/proof/2017-05-03/5909458ee180a.jpg","store_name":"m3mohanman","sum_money":"2326212.00","user_id":"1908"},{"photo":"/Uploads/proof/2017-08-17/5994fd7f60652.jpg","store_name":"建发医药连锁店","sum_money":"2277408.00","user_id":"2702"},{"photo":"/Uploads/proof/2017-08-16/5993d1fbb202c.jpg","store_name":"高邮市永庆综合建材门市部","sum_money":"2162132.00","user_id":"1990"},{"photo":"/Uploads/proof/2017-08-16/5993c101e2f1f.jpg","store_name":"常熟市服装城仙华日用百货商行","sum_money":"2123104.00","user_id":"2670"},{"photo":"/Uploads/proof/2017-08-15/599255facbbaf.jpg","store_name":"妙芝琳服装店","sum_money":"2115200.00","user_id":"2621"},{"photo":"/Uploads/proof/2017-08-15/5992786940626.jpg","store_name":"常熟市古里镇辉云门窗经营部","sum_money":"2085504.00","user_id":"2572"},{"sum_money":"2040000.00","user_id":"8894"},{"photo":"","store_name":"哒哒食品店","sum_money":"2024880.00","user_id":"2044"},{"photo":"/Uploads/proof/2017-08-16/59940627f0971.jpg","store_name":"江都区明川百货便利店","sum_money":"2022260.00","user_id":"3463"},{"photo":"/Uploads/proof/2017-05-01/590741bc8b908.jpg","store_name":"金门楼饭店","sum_money":"2019044.00","user_id":"1753"},{"photo":"/Uploads/proof/2017-06-04/5933856fd2eee.jpg","store_name":"宫予家纺","sum_money":"1966400.00","user_id":"2071"},{"photo":"/Uploads/proof/2017-08-18/59964d9ee52b9.jpg","store_name":"江都区邵伯镇广霖江业百货超市","sum_money":"1945284.00","user_id":"557"},{"photo":"/Uploads/proof/2017-08-21/599a49e4b3350.jpg","store_name":"毛毛熟菜店","sum_money":"1933008.00","user_id":"4393"},{"photo":"/Uploads/proof/2017-05-31/592e9d7b457b4.jpg","store_name":"杭州高瑞医疗器械有限公司","sum_money":"1907191.00","user_id":"4324"},{"photo":"/Uploads/proof/2017-08-22/599bd8a79f8d4.jpg","store_name":"江都区仙女镇荣登装饰建材经营部","sum_money":"1902411.00","user_id":"2079"},{"photo":"/Uploads/proof/2017-08-23/599d8c5aee294.JPG","store_name":"艾美庄园","sum_money":"1897908.00","user_id":"2151"},{"photo":"/Uploads/proof/2017-08-16/599435e8d126c.JPG","store_name":"金裕商行（天叶烟酒）","sum_money":"1891104.00","user_id":"3996"},{"photo":"/Uploads/proof/2017-08-21/599a5173d413d.JPG","store_name":"丹阳市越丰车业有限公司","sum_money":"1791700.00","user_id":"3268"},{"photo":"","store_name":"好屋来皮鞋店","sum_money":"1782000.00","user_id":"4429"},{"photo":"/Uploads/proof/2017-08-20/59993da287aa9.jpg","store_name":"张家港市名仕纺织厂","sum_money":"1775362.00","user_id":"2809"},{"photo":"/Uploads/proof/2017-08-16/5993bafe7e0e7.jpg","store_name":"高邮市龙腾手机有限公司","sum_money":"1759168.00","user_id":"3309"},{"photo":"/Uploads/proof/2017-05-09/59109ad54776d.png","store_name":"树荣日用百货","sum_money":"1758400.00","user_id":"6939"},{"photo":"/Uploads/proof/2017-08-19/5997dfc0d6049.JPG","store_name":"君昊美容","sum_money":"1728682.00","user_id":"4730"},{"photo":"/Uploads/proof/2017-08-18/599656a5dbb0d.jpg","store_name":"南北货","sum_money":"1724000.00","user_id":"5360"},{"photo":"/Uploads/proof/2017-08-16/5993bfabdda28.jpg","store_name":"无锡市人本装饰公司","sum_money":"1689508.00","user_id":"1775"},{"photo":"/Uploads/proof/2017-08-16/5993ace9a922e.jpg","store_name":"苏州阳光雨防水建材有限公司","sum_money":"1608864.00","user_id":"2725"},{"photo":"/Uploads/proof/2017-08-15/599289cb01260.jpg","store_name":"江都区樊川明俊烟花爆竹店","sum_money":"1607300.00","user_id":"2869"},{"photo":"/Uploads/proof/2017-08-16/5993cd6b1d69f.JPG","store_name":"***","sum_money":"1588516.00","user_id":"3028"},{"photo":"/Uploads/proof/2017-08-22/599bda9996027.jpg","store_name":"江都区仙女镇荣登装饰建材经营部","sum_money":"1564408.00","user_id":"4384"},{"photo":"/Uploads/proof/2017-05-02/590879a7bcf1e.jpg","store_name":"南京健宝仕科技医药有限公司","sum_money":"1552320.00","user_id":"2080"},{"photo":"/Uploads/proof/2017-08-18/5996703f85db0.jpg","store_name":"恒盛织造","sum_money":"1521600.00","user_id":"2758"},{"photo":"/Uploads/proof/2017-08-15/599253066d04d.jpg","store_name":"幸运鸟文体用品店","sum_money":"1521168.00","user_id":"2681"},{"photo":"/Uploads/proof/2017-08-16/5993ba05392b7.jpg","store_name":"秋林百货店","sum_money":"1517666.00","user_id":"1857"},{"photo":"/Uploads/proof/2017-08-18/5996832e4f8be.jpg","store_name":"常熟市美华服饰","sum_money":"1516848.00","user_id":"3505"},{"photo":"/Uploads/proof/2017-05-12/5915534b133d5.jpg","store_name":"江苏省无锡市南禅寺妙光街134号A景悦珠宝","sum_money":"1477157.15","user_id":"2839"},{"photo":"/Uploads/proof/2017-04-12/58edf34ba5be9.PNG","store_name":"吉祥树旗舰店","sum_money":"1447730.00","user_id":"282"},{"photo":"/Uploads/proof/2017-05-01/5906f8c6b32b2.jpg","store_name":"钱盛物联网无锡分公司","sum_money":"1444340.00","user_id":"1740"},{"photo":"/Uploads/proof/2017-08-16/5993d81393a35.jpg","store_name":"格瑞特橱柜衣柜移门店","sum_money":"1413200.00","user_id":"2746"},{"photo":"/Uploads/proof/2017-08-15/599286e990613.jpg","store_name":"江都区全福饭店","sum_money":"1410260.00","user_id":"4097"},{"photo":"/Uploads/proof/2017-08-18/59968877f33d4.jpg","store_name":"新日","sum_money":"1400065.00","user_id":"3089"},{"photo":"/Uploads/proof/2017-04-11/58eca29a55a7d.png","store_name":"韩之美味","sum_money":"1396387.00","user_id":"194"},{"photo":"/Uploads/proof/2017-08-16/5993bbd30e827.jpg","store_name":"雪英男装店","sum_money":"1376546.00","user_id":"713"},{"photo":"/Uploads/proof/2017-08-17/59953ace78c5c.jpg","store_name":"南通宫予家纺有限公司","sum_money":"1370800.00","user_id":"6984"},{"photo":"/Uploads/proof/2017-08-19/599840ede1fbf.jpg","store_name":"友谊商店","sum_money":"1359600.00","user_id":"4549"},{"photo":"/Uploads/proof/2017-08-24/599e2ebc432cb.jpg","store_name":"张家港市凤凰镇子菲汗蒸馆","sum_money":"1358064.00","user_id":"3981"},{"photo":"/Uploads/proof/2017-08-17/5995488eb0412.PNG","store_name":"防火门公司","sum_money":"1350877.00","user_id":"2275"},{"photo":"/Uploads/proof/2017-08-22/599be6a76a79b.jpg","store_name":"江都区邵伯镇枫乐居大酒店","sum_money":"1329802.00","user_id":"2537"},{"photo":"/Uploads/proof/2017-05-11/591412b540d53.jpg","store_name":"完美服装","sum_money":"1315728.00","user_id":"8007"},{"photo":"/Uploads/proof/2017-08-22/599bb014a18c7.jpg","store_name":"他二舅手工水饺店","sum_money":"1309968.00","user_id":"2956"},{"photo":"/Uploads/proof/2017-08-20/5999347e871b2.jpg","store_name":"尚夏包装","sum_money":"1305216.00","user_id":"3072"},{"photo":"","store_name":"123456","sum_money":"1301750.00","user_id":"4763"},{"photo":"","store_name":"萍萍小屋","sum_money":"1293600.00","user_id":"3614"},{"photo":"/Uploads/proof/2017-05-13/59167334d8a47.jpeg","store_name":"韦恩艺术画廊","sum_money":"1276148.00","user_id":"3849"},{"photo":"/Uploads/proof/2017-04-12/58edf5edaddbb.JPG","store_name":"红酒雅韵","sum_money":"1273870.00","user_id":"286"},{"photo":"/Uploads/proof/2017-08-15/5992fd70c91f8.jpg","store_name":"高邮市伙伴渔药门市部","sum_money":"1265060.00","user_id":"1894"},{"photo":"/Uploads/proof/2017-08-16/59944103ad610.jpg","store_name":"如家酒店","sum_money":"1253472.00","user_id":"4165"},{"photo":"/Uploads/proof/2017-08-17/599501451cae1.jpg","store_name":"镇江市润州区施进江鲜鱼馆","sum_money":"1252004.00","user_id":"2263"},{"photo":"/Uploads/proof/2017-05-02/59087e8e55feb.jpg","store_name":"菜根香酒店","sum_money":"1232272.00","user_id":"2427"}]}
+     * status : 1
+     * succ : 1
+     */
+    private int i;
+    private List<SingleLogBean> single_log;
+
+    public int getI() {
+        return i;
+    }
+
+    public void setI(int i) {
+        this.i = i;
+    }
+
+    public List<SingleLogBean> getSingle_log() {
+        return single_log;
+    }
+
+    public void setSingle_log(List<SingleLogBean> single_log) {
+        this.single_log = single_log;
+    }
+
+    public static class SingleLogBean {
+        /**
+         * sum_money : 11039330.00
+         * user_id : 8895
+         * photo : /Uploads/proof/2017-08-16/5993f0941f66f.jpg
+         * store_name : 常熟市古里镇恒洁卫浴经营部
+         */
+
+        private String sum_money;
+        private String user_id;
+        private String photo;
+        private String store_name;
+
+        public String getSum_money() {
+            return sum_money;
+        }
+
+        public void setSum_money(String sum_money) {
+            this.sum_money = sum_money;
+        }
+
+        public String getUser_id() {
+            return user_id;
+        }
+
+        public void setUser_id(String user_id) {
+            this.user_id = user_id;
+        }
+
+        public String getPhoto() {
+            return photo;
+        }
+
+        public void setPhoto(String photo) {
+            this.photo = photo;
+        }
+
+        public String getStore_name() {
+            return store_name;
+        }
+
+        public void setStore_name(String store_name) {
+            this.store_name = store_name;
+        }
+    }
+}
