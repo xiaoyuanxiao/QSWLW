e942a6212c86f715579d1b01004161091cfb57b3
xiaoyu <2640171401@qq.com>
2017-09-05 13:49:30 +0800

    消费者个人中心

--- app/src/main/java/com/qs/qswlw/adapter/ConsumerSettingAdapter.java
+++ app/src/main/java/com/qs/qswlw/adapter/ConsumerSettingAdapter.java
@@ -24,15 +24,16 @@ public class ConsumerSettingAdapter extends BaseAdapter {
         this.context = context;
     }
 
-    public int[] imgs = {R.mipmap.beans, R.mipmap.xfjd, R.mipmap.xfyd,
-            R.mipmap.wdjb, R.mipmap.tz, R.mipmap.cyzz, R.mipmap.wytj,
-            R.mipmap.tx,R.mipmap.scdd,R.mipmap.cyzz,R.mipmap.zz,
-            R.mipmap.cj,R.mipmap.user_address};
+    public int[] imgs = {R.mipmap.beans, R.mipmap.xfyd, R.mipmap.cyzz,
+            R.mipmap.wdjb, R.mipmap.tz, R.mipmap.cyzj, R.mipmap.wytj,
+            R.mipmap.tx,R.mipmap.user08,R.mipmap.cj,R.mipmap.user_address,
+            R.mipmap.jilu,R.mipmap.shouh,R.mipmap.tx,R.mipmap.daizi,R.mipmap.citie,R.mipmap.jinbi,
+            R.mipmap.saoma,R.mipmap.consume_quota};
 
-    public String[] contents = {"创业金豆", "消费金豆", "消费银豆", "我的金币",
-            "我的投资", "创业种子", "我要推荐", "我要提现","商城订单",
-            "创业直捐","我要转增","促销抽奖记录","收获地址"};
-
+    public String[] contents = {"我的金豆", "消费银豆", "创业种子", "我的金币",
+            "我的投资", "创业直捐", "我要推荐", "我要提现","我要转赠",
+            "促销抽奖记录","收货地址","升级申请","获赠记录","商品评价",
+            "我的购物","我的商城设置","商城消费金豆专区","线下门店扫码","我的阿消费额度"};
     @Override
     public int getCount() {
         return imgs.length;
