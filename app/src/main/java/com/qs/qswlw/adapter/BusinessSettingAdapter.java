1cb8f7965173016a51b02c9b9b471d1391174f89
deepcummer <750264859@qq.com>
2017-12-25 16:43:01 +0800

    提现

--- app/src/main/java/com/qs/qswlw/adapter/BusinessSettingAdapter.java
+++ app/src/main/java/com/qs/qswlw/adapter/BusinessSettingAdapter.java
@@ -17,7 +17,7 @@ import com.qs.qswlw.R;
  * 设置中心的adapter
  */
 public class BusinessSettingAdapter extends BaseAdapter {
-    public String[] contents={"商家录单","我要推荐","客户消费记录","我的机构","商城订单","现金专区","消费金豆专区","收货地址","消费银豆","创业种子",
+    public String[] contents={"商家录单","我要推荐","客户消费记录","我的机构","商城订单","自营商品","现金专区","消费金豆专区","收货地址","消费银豆","创业种子",
     "我的金豆","我的奖金","我要提现","促销抽奖记录","我的角色","商家完善资料","商品管理","我的商城","商家营业额"};
 
 //    public String[] contents = {"商家二维码", "门店扫码记录", "商家扫码审核", "商家销售审核",
@@ -32,7 +32,7 @@ public class BusinessSettingAdapter extends BaseAdapter {
 //            R.mipmap.beans,R.mipmap.tx,R.mipmap.daizi,R.mipmap.user_address,R.mipmap.jinbi,
 //            R.mipmap.jinbi2,R.mipmap.consume_quota,R.mipmap.citie,R.mipmap.user08,R.mipmap.shouh,
 //            R.mipmap.wdjb,R.mipmap.cyzj,R.mipmap.wslhy,R.mipmap.user_ren, R.mipmap.cyzj,R.mipmap.tubiao0028,R.mipmap.cj,R.mipmap.huishou};
-    public int[] ids = {R.mipmap.user_09,R.mipmap.wytj,R.mipmap.jilu,R.mipmap.tubiao0030,R.mipmap.daizi,R.mipmap.jinbi,R.mipmap.jinbi2,
+    public int[] ids = {R.mipmap.user_09,R.mipmap.wytj,R.mipmap.jilu,R.mipmap.tubiao0030,R.mipmap.daizi,R.mipmap.is_me,R.mipmap.jinbi,R.mipmap.jinbi2,
             R.mipmap.user_address,R.mipmap.xfyd,R.mipmap.cyzz,R.mipmap.xfjd,R.mipmap.xfjd,R.mipmap.tx,R.mipmap.cj,R.mipmap.user_ren,
             R.mipmap.bianji,R.mipmap.spgl,R.mipmap.lingdang,R.mipmap.yye};
 
